4d4b2b7574c90acca10fbcc0354b83f9bc3b0f3e
AAntoniazzi <aramys.antoniazzi@gmail.com>
2020-08-25 20:19:20 -0300

    ajuste classe

--- src/Fatura/principal.java
+++ src/Fatura/principal.java
@@ -2,9 +2,9 @@ package Fatura;
 
 public class principal {
     public static void main(String[] args) {
-        int qt = Integer.parseInt(args[2]);
-        float preco = Float.parseFloat(args[3]);
-
+        int qt = Integer.parseInt(args[0]);
+        float preco = Float.parseFloat(args[1]);
+        //quantidade, preco, codigo, descricao ex: 2, 7, L22, lapis
         Fatura fatura1 = new Fatura(qt, preco, args[2], args[3]);
         fatura1.imprime();
     }
