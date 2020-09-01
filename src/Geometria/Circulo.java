ed2d975f9b1fedc7bbbe9bc15e0d5d0e4f29cf2c
AAntoniazzi <aramys.antoniazzi@gmail.com>
2020-09-01 20:52:12 -0300

    criando classe circulo e quadrado

---
+++ src/Geometria/Circulo.java
@@ -0,0 +1,42 @@
+package Geometria;
+
+public class Circulo extends Forma {
+    protected double raio;
+
+    public Circulo() {
+        super();
+    }
+
+    public Circulo(double raio) {
+        super();
+        this.setRaio(raio);
+    }
+
+    public Circulo(double raio, String cor, boolean preenchido) {
+        super(cor, preenchido);
+        this.setRaio(raio);
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                ", cor=" + cor +
+                ", preenchido" + preenchido +
+                '}';
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double getArea() {
+        double pi = 3.1416;
+        return pi * (raio * raio);
+    }
+}
